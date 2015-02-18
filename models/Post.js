b51e1bed5ef0bfeef8207770779e9864b2c94c12
nicolas silva <sovietfrance@gmail.com>
2015-02-18 17:02:16 -0300

    prepare File for API calls, ignore fields, and ADD get Route

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,7 @@
 var keystone = require('keystone'),
-	Types = keystone.Field.Types;
+	Types = keystone.Field.Types,
+	keystoneRest = require('keystone-rest');
+
 
 /**
  * Post Model
@@ -13,15 +15,15 @@ var Post = new keystone.List('Post', {
 
 Post.add({
 	title: { type: String, required: true },
-	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	author: { type: Types.Relationship, ref: 'User', index: true },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true, restSelected: false },
+	author: { type: Types.Relationship, ref: 'User', index: true, restSelected: false},
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-	image: { type: Types.CloudinaryImage },
+	image: { type: Types.CloudinaryImage, restSelected: false },
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 }
+		extended: { type: Types.Html, wysiwyg: true, height: 400, restSelected: false},
 	},
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true }
+	categories: { type: Types.Relationship, ref: 'PostCategory', many: true, restSelected: false }
 });
 
 Post.schema.virtual('content.full').get(function() {
@@ -29,4 +31,15 @@ Post.schema.virtual('content.full').get(function() {
 });
 
 Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
+
+
 Post.register();
+
+// Expose User model via REST api
+keystoneRest.addRoutes(Post, 'get');
+
+
+
+
+
+
